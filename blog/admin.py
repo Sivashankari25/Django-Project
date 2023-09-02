91998d866b9ef1f9bc824284b150b58d871718ca
SIVASHANKARI S <117724196+Sivashankari25@users.noreply.github.com>
2023-09-02 16:54:28 +0530

    Delete blog directory

--- blog/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Blog  #class Name
-
-admin.site.register(Blog)  #class Name
-
-# Register your models here.
--- blog/forms.py
+++
@@ -1,8 +0,0 @@
-from django import	forms
-from django.forms import ModelForm
-from .models import Blog     # class name in models to import
-
-class BlogForm(forms.ModelForm):
-	class Meta:
-		model = Blog         #class name in models
-		fields = "__all__" 
